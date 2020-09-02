63d8d40c72b4c94800b7da97357daf5d021a4844
papalooo <papalooo@naver.com>
2020-09-02 19:41:07 +0900

    c++
    List

--- Project3/List.cpp
+++ Project3/List.cpp
@@ -35,3 +35,101 @@ using namespace std;
 // lt.sort(pred)			lt 의 모든 원소를 pred 기준으로 정렬
 // lt.splice(p, lt2)		p가 가르키는 위치에 lt2의 모든 원소를 잘라붙임
 // lt.swap(lt2)				lt와 lt2의 원소를 교체
+// lt.unique()				인접한 원소의 값이 같다면 유일한 원소의 순차열로 만듦
+#include <list>	
+
+void main()
+{
+	list<int> lt1;
+	list<int> lt2;
+
+	lt1.push_back(10);
+	lt1.push_back(20);
+	lt1.push_back(30);
+	lt1.push_back(40);
+	lt1.push_back(50);
+
+	lt2.push_back(100);
+	lt2.push_back(200);
+	lt2.push_back(300);
+	lt2.push_back(400);
+	lt2.push_back(500);
+
+
+	list<int>::iterator iter;
+	cout << "lt1 : ";
+	for (iter = lt1.begin(); iter != lt1.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	cout << "lt2 : ";
+	for (iter = lt2.begin(); iter != lt2.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+
+	iter = lt1.begin();
+	++iter; // 20
+	++iter; // 30
+
+	lt1.splice(iter, lt2);	// iter 가 가르키는 lt의 위치에 lt2의 모든 원소를 잘라 붙임
+
+	cout << "splice lt1 : ";	// lt1 의 30자리부터 lt2 가 잘라 붙여짐
+	for (iter = lt1.begin(); iter != lt1.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	cout << "splice lt2 : ";	// lt2는 잘라져서 비어있음
+	for (iter = lt2.begin(); iter != lt2.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	lt1.reverse(); // 리스트를 역순으로 뒤집음.
+	cout << "reverse lt1 : ";
+	for (iter = lt1.begin(); iter != lt1.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	lt1.push_front(50);
+	lt1.push_front(50);
+	lt1.push_back(10);
+	lt1.push_back(10);
+	cout << "r push lt1 : ";
+	for (iter = lt1.begin(); iter != lt1.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+
+	lt1.unique();		// 인접한 중복원소를 합침 (앞뒤로만 같으면 합침)
+	//lt1.push_front(500);
+	lt1.unique();
+	cout << "unique lt1 : ";
+	for (iter = lt1.begin(); iter != lt1.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+	lt1.sort();		// 오름차순 정렬
+	cout << "sort lt1 : ";
+	for (iter = lt1.begin(); iter != lt1.end(); iter++)
+	{
+		cout << *iter << " ";
+	}
+	cout << endl;
+
+}
+
+
